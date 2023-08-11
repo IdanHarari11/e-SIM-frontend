71dd34bf6f723ea9d887fbdb0b5a35194d13bd4c
Idan Harari <idanharari112@gmail.com>
2023-08-11 19:18:33 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,32 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) },
-  // More routes here
+  {
+    path: 'auth',
+    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
+  },
+  {
+    path: 'sales',
+    loadChildren: () => import('./sales/sales.module').then(m => m.SalesModule)
+  },
+  {
+    path: 'subscription',
+    loadChildren: () => import('./subscription/subscription.module').then(m => m.SubscriptionModule)
+  },
+  {
+    path: 'support',
+    loadChildren: () => import('./support/support.module').then(m => m.SupportModule)
+  },
+  {
+    path: '',   // default route
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**', // wildcard for unmatched routes
+    redirectTo: '/home'
+  }
+  // Add more lazy-loaded routes as needed
 ];
 
 @NgModule({
